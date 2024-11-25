58b1a5f34f5e1daeeac9aad4a8331a62ebb3eda4
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-11-26 03:35:51 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        while k != 0:
+            nums, k = self.find(nums, k)
+            print(nums, k)
+        return nums[0]
+
+    def find(self, nums: list[int], k: int) -> tuple[list[int], int]:
+        left, right, mid = list(), list(), list()
+        limit = nums[0]
+        for num in nums:
+            if num > limit:
+                right.append(num)
+            elif num < limit:
+                left.append(num)
+            else:
+                mid.append(num)
+        len_right, len_mid = len(right), len(mid)
+        if len_right >= k:
+            return right, k
+        elif len_right + len_mid >= k:
+            return [limit], 0
+        else:
+            return left, k - len_right - len_mid
